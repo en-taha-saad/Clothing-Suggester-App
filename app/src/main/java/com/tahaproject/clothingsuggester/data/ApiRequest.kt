7814cb8a3c443bed8a45af7d53dbe2d2f61e3dbd
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-04-16 06:23:24 +0300

    make current weather request

--- app/src/main/java/com/tahaproject/clothingsuggester/data/ApiRequest.kt
+++ app/src/main/java/com/tahaproject/clothingsuggester/data/ApiRequest.kt
@@ -3,6 +3,7 @@ package com.tahaproject.clothingsuggester.data
 import com.google.gson.Gson
 import com.tahaproject.clothingsuggester.util.Constants
 import okhttp3.FormBody
+import okhttp3.HttpUrl
 import okhttp3.Request
 import okhttp3.logging.HttpLoggingInterceptor
 
@@ -12,21 +13,23 @@ open class ApiRequest {
         level = HttpLoggingInterceptor.Level.BODY
     }
 
-    private fun makeRequest(endPoint: String): Request.Builder {
-        return Request.Builder()
-            .url("${Constants.URL}/$endPoint")
+    private fun getUrlBuilder(lat: String, lon: String, appid: String): HttpUrl {
+        return HttpUrl.Builder()
+            .scheme(Constants.API_SCHEME)
+            .host(Constants.API_HOST)
+            .addPathSegment(Constants.API_DATA_SEGMENT)
+            .addPathSegment(Constants.API_VERSION_SEGMENT)
+            .addPathSegment(Constants.API_FORECAST_SEGMENT)
+            .addQueryParameter("lat", lat)
+            .addQueryParameter("lon", lon)
+            .addQueryParameter("appid", appid)
+            .addQueryParameter("units", Constants.API_DEFAULT_UNITS)
+            .build()
     }
 
-    open fun postRequest(body: FormBody, endPoint: String): Request =
-        makeRequest(endPoint).post(
-            body
-        ).build()
+    private fun getCurrentWeather(lat: String, lon: String, appid: String): Request.Builder {
+        return Request.Builder()
+            .url(getUrlBuilder(lat, lon, appid))
+    }
 
-
-    open fun getRequest(endPoint: String): Request = makeRequest(endPoint).get().build()
-
-    open fun putRequest(body: FormBody, endPoint: String): Request =
-        makeRequest(endPoint).put(
-            body
-        ).build()
 }
