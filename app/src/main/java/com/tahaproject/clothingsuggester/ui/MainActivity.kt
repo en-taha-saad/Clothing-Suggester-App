5022321101e8420749f8914c46b3d055d6cb47bd
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-04-16 06:46:15 +0300

    change activity location to ui

--- app/src/main/java/com/tahaproject/clothingsuggester/ui/MainActivity.kt
+++ app/src/main/java/com/tahaproject/clothingsuggester/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.tahaproject.clothingsuggester
+package com.tahaproject.clothingsuggester.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tahaproject.clothingsuggester.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
