2ff2768383c6695bce42f38d16b036de13ffb93c
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-04-16 07:58:35 +0300

    make the current weather api request

--- app/src/main/java/com/tahaproject/clothingsuggester/ui/MainActivity.kt
+++ app/src/main/java/com/tahaproject/clothingsuggester/ui/MainActivity.kt
@@ -8,21 +8,41 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import com.tahaproject.clothingsuggester.BuildConfig
 import com.tahaproject.clothingsuggester.R
+import com.tahaproject.clothingsuggester.data.ApiRequest
+import com.tahaproject.clothingsuggester.data.FakeDataGenerator
+import com.tahaproject.clothingsuggester.data.WeatherModelImpl
 import com.tahaproject.clothingsuggester.data.models.requests.Location
 import com.tahaproject.clothingsuggester.data.models.response.WeatherData
 
 
 class MainActivity : AppCompatActivity(), WeatherView {
 
-    private val apiKey = "YOUR_API_KEY"
+    private lateinit var presenter: WeatherPresenter
+    private lateinit var apiRequest: ApiRequest
+    private lateinit var weatherData: WeatherData
+    private lateinit var requiredLocation: Location
+    private val apiKey = BuildConfig.apiKey
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        apiRequest = ApiRequest()
+        presenter = WeatherPresenterImpl(
+            this,
+            WeatherModelImpl(
+                apiRequest, apiKey
+            )
+        )
         requestLocation()
+//        (presenter as WeatherPresenterImpl).getCurrentWeatherData(requiredLocation.lat, requiredLocation.lon)
+        weatherData = FakeDataGenerator.generateWeatherData()
+
+
         // TODO: make the xml
 //        fetchWeatherButton.setOnClickListener {
 //            val location = locationEditText.text.toString()
@@ -69,16 +89,24 @@ class MainActivity : AppCompatActivity(), WeatherView {
             // for ActivityCompat#requestPermissions for more details.
             fusedLocationClient.lastLocation.addOnSuccessListener { location: android.location.Location? ->
                 location?.let {
-                    val locationString =
+                    requiredLocation =
                         Location(
                             it.latitude.toString(),
                             it.longitude.toString()
                         )
-                    getCurrentWeather(locationString)
                 } ?: showError("Failed to get location")
             }
             return
         }
     }
 
+
+    override fun showCurrentWeatherData(weatherData: WeatherData) {
+//        temperatureTextView.text = "Temperature: ${weatherData.temperature}"
+//        descriptionTextView.text = "Description: ${weatherData.description}"
+    }
+
+    override fun showError(errorMessage: String) {
+//        errorTextView.text = errorMessage
+    }
 }
