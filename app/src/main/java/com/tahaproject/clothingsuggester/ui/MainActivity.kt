184f1b7f1f103aec72a0d78628860bd3f1fddd3c
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-04-18 01:16:37 +0300

    completing the logic

--- app/src/main/java/com/tahaproject/clothingsuggester/ui/MainActivity.kt
+++ app/src/main/java/com/tahaproject/clothingsuggester/ui/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.tahaproject.clothingsuggester.ui
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
+import android.location.Geocoder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.gms.location.FusedLocationProviderClient
@@ -15,13 +19,16 @@ import com.tahaproject.clothingsuggester.data.FakeDataGenerator
 import com.tahaproject.clothingsuggester.data.WeatherModelImpl
 import com.tahaproject.clothingsuggester.data.models.requests.Location
 import com.tahaproject.clothingsuggester.data.models.response.WeatherData
+import com.tahaproject.clothingsuggester.databinding.ActivityMainBinding
+import java.util.*
 
 
 class MainActivity : AppCompatActivity(), IWeatherView {
-
+    private lateinit var binding: ActivityMainBinding
     private lateinit var presenter: WeatherPresenter
     private lateinit var apiRequest: ApiRequest
-    private lateinit var weatherData: WeatherData
+    private lateinit var weatherModelImpl: WeatherModelImpl
+
     private lateinit var requiredLocation: Location
     private val apiKey = BuildConfig.apiKey
     private lateinit var fusedLocationClient: FusedLocationProviderClient
@@ -29,23 +36,16 @@ class MainActivity : AppCompatActivity(), IWeatherView {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         apiRequest = ApiRequest()
-        presenter = WeatherPresenter(
-            this,
-            WeatherModelImpl(
-                apiRequest, apiKey
-            )
-        )
-        weatherData = FakeDataGenerator.generateWeatherData()
-//        presenter.getCurrentWeatherData(requiredLocation)
+        weatherModelImpl = WeatherModelImpl(apiRequest, apiKey)
+        presenter = WeatherPresenter(this, weatherModelImpl)
+//        weatherData = FakeDataGenerator.generateWeatherData()
 
         // Initialize FusedLocationProviderClient for Location Services
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-
         requestLocationPermission()
-        // TODO: make the xml
-        // setUp()
     }
 
     private fun requestLocationPermission() {
@@ -77,31 +77,50 @@ class MainActivity : AppCompatActivity(), IWeatherView {
         }
     }
 
+    private fun getAddressFromLocation(latitude: Double, longitude: Double): String {
+        val geocoder = Geocoder(this, Locale.getDefault())
+        val addresses = geocoder.getFromLocation(latitude, longitude, 1)
+        val address = addresses?.firstOrNull()
+        return address?.getAddressLine(0) ?: "Unknown Location"
+    }
 
+
+    @SuppressLint("MissingPermission")
     private fun getCurrentLocation() {
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            fusedLocationClient.lastLocation.addOnSuccessListener { location: android.location.Location? ->
-                location?.let {
-                    presenter.getCurrentWeatherData(Location(it.latitude.toString(), it.longitude.toString()))
-                } ?: showError("Failed to get location")
-            }
-            return
+        fusedLocationClient.lastLocation.addOnSuccessListener { location: android.location.Location? ->
+            location?.let {
+                val address = getAddressFromLocation(it.latitude, it.longitude)
+                presenter.getCurrentWeatherData(Location(it.latitude.toString(), it.longitude.toString()))
+
+                updateUI(address)
+            } ?: showError("Failed to get location")
+        }
+
+    }
+
+    private fun updateUI(address: String) {
+        Log.i("Tag", address)
+//        binding.locationTextView.text = address
+    }
+
+
+    private fun updateUI2(weatherData: WeatherData) {
+        runOnUiThread {
+            binding.locationTextView.text = weatherData.forecasts.size.toString()
         }
     }
 
+
     override fun showCurrentWeatherData(weatherData: WeatherData) {
+        updateUI2(weatherData)
 //        temperatureTextView.text = "Temperature: ${weatherData.temperature}"
 //        descriptionTextView.text = "Description: ${weatherData.description}"
     }
 
     override fun showError(errorMessage: String) {
-//        errorTextView.text = errorMessage
+        // errorTextView.text = errorMessage
+        // make a toast
+        Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
+
     }
 }
